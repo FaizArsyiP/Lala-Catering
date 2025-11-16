9783365b45ea651758b115864b540816e0d03735
FaizArsyiP <faizpragata@gmail.com>
2025-11-16 22:04:07 +0700

    fix: update orderList and useOrders types for improved data handling

--- lala-web/src/hooks/useOrders.tsx
+++ lala-web/src/hooks/useOrders.tsx
@@ -30,6 +30,7 @@ export type Order = {
     };
     userId: string;
     deliveries: Delivery[];
+    items: DeliveryItem[];
     totalHarga: number;
     alamatPengirimanText: string;
     metodePengambilan: string;
