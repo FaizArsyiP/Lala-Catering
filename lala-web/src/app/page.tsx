698d7ce20b157f92c1797b43c40f5bf9352e125e
FaizArsyiP <faizpragata@gmail.com>
2025-11-17 11:31:21 +0700

    feat: refactor HomePage layout and remove unused profile page

--- lala-web/src/app/page.tsx
+++ lala-web/src/app/page.tsx
@@ -7,29 +7,12 @@ import { todayMenu as TodayMenu } from "@/components/layout/landingPage/todayMen
 
 export default function HomePage() {
     return (
-<<<<<<< HEAD
         <main className="w-full min-h-screen bg-[#002683]">
             <Header />
             <HeroSection />
             <ProfileSection />
             <div className="flex justify-center items-center">
-              <TodayMenu />
-=======
-        <main className="overflow-x-hidden w-screen h-screen bg-[#002683] flex justify-center items-center">
-            {/* COMPONENTS CONTAINER */}
-            <div
-                className="w-full h-full 
-                            xs:mx-[30px] 
-                            tablet:mx-[60px] 
-                            md:mx-[80px] 
-                            lg:mx-[100px] 
-                            xl:mx-[130px] 
-                            desk:mx-[150px]">
-                {/* HEADER */}
-                <div className="w-full">
-                    <Header />
-                </div>
->>>>>>> origin/main
+                <TodayMenu />
             </div>
         </main>
     );
