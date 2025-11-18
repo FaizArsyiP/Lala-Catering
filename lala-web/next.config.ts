d1bae28663c9297f58d0b068b385710d6f6cf4dc
FaizArsyiP <faizpragata@gmail.com>
2025-11-18 15:56:04 +0700

    fix: reorder eslint and images configuration in next.config.ts for clarity

--- lala-web/next.config.ts
+++ lala-web/next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-    ],
-  },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
